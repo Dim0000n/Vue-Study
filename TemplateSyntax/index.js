bf3e3eda7a2f8fcccb2fadbf7e6268b4cea3001b
Dimon <yoh60438@gmail.com>
2022-10-18 00:41:09 +0700

    Finish to study TemplateSyntax section'

--- TemplateSyntax/index.js
+++ TemplateSyntax/index.js
@@ -46,3 +46,51 @@ new Vue({
         } 
     }
 })
+
+//JavaScript выражения
+new Vue({
+    el:"#app-5",
+    data: {
+        number: 5,
+        ok: true,
+        message: "This is a message.",
+        id: 3
+    },
+    methods: {
+        changeValue: function() {
+            this.ok = !this.ok
+        }
+    }
+})
+
+//Динамические аргументы
+//поддерживаются версией 2.6.0+
+new Vue({
+    el: "#app-6",
+    data: {
+        attributeName: "href",
+        url: "https://ru.vuejs.org/v2/guide/syntax.html"
+    }
+})
+
+//Сокращения
+//Сокращение v-bind
+new Vue({
+    el:"#app-7",
+    data: {
+        url: "https://ru.vuejs.org/v2/guide/syntax.html",
+        key: "href"
+    }
+})
+//сокращение v-on
+new Vue({
+    el:"#app-8",
+    data: {
+        event: "click"
+    },
+    methods: {
+        getAlert: function() {
+            alert("You've just clicked the Link");
+        }
+    }
+})
