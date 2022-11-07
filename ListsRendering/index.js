9c004cf610ec43204b217c98f0484c2ccc9c4047
Dimon <yoh60438@gmail.com>
2022-11-07 18:03:12 +0700

    Continue of ListRendering studying

--- ListsRendering/index.js
+++ ListsRendering/index.js
@@ -43,3 +43,43 @@ new Vue({
         }
     }
 })
+
+//отображение отфильтрованных/отортированных выражений
+new Vue({
+    el: "#app-4",
+    data: {
+        numbers: [1, 2, 3, 4, 5],
+        sets: [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+    },
+    computed: {
+        evenNumbers: function () {
+            return this.numbers.filter(function (number) {
+                return number % 2 === 0
+            })
+        }
+    },
+    methods: {
+        even: function (set) {
+            return set.filter(function (number) {
+                return number % 2 === 0
+            })
+        }
+    }
+})
+
+//v-for и диапазоны
+new Vue({
+  el: "#app-5"  
+})
+
+//v-for и template
+new Vue({
+    el: "#app-6",
+    data: {
+        check: "kek",
+        items: [
+            { msg: "Foo" },
+            { msg: "Bar" }
+        ]
+    }
+})
