f1c75985f1c1259e26f9a130f01082a9d450ce96
Dimon <yoh60438@gmail.com>
2022-11-17 17:22:19 +0700

    Started to study Forms

---
+++ Forms/index.js
@@ -0,0 +1,25 @@
+//text
+new Vue({
+    el: "#app-1",
+    data: {
+        message: "",
+        ml_message: ""
+    }
+})
+
+//checkboxes
+new Vue({
+    el: "#app-2",
+    data: {
+        checked: false,
+        checkedNames: []
+    }
+})
+
+//radiobuttons
+new Vue({
+    el: "#app-3",
+    data: {
+        picked: ""
+    }
+})
