4e1d532e9764007d705e742c687ed94b860acfa2
Dimon <yoh60438@gmail.com>
2022-12-04 23:32:06 +0700

    continue of study baseofcomponents

--- BaseOfComponents/index.js
+++ BaseOfComponents/index.js
@@ -16,6 +16,8 @@ Vue.component('blog-post', {
     template: '<p> {{ title }} </p>'
 })
 
+
+
 new Vue({
     el: "#app-2",
     data : {
@@ -26,3 +28,87 @@ new Vue({
         ]
     }
 })
+
+//EVENTS
+//events listening
+Vue.component('blog-post-events', {
+    props: ['post'],
+    template: '<div class="blog-post">' +
+        '<h3>{{ post.title }}</h3>' +
+        '<button v-on:click="$emit(\'enlarge-text\')">' +
+            'Increase font-size' +
+        '</button>' +
+        '<div v-html="post.content"></div>' +
+    '</div>'
+})
+
+//Emit data in event
+Vue.component('blog-post-events-data', {
+    props: ['post'],
+    template: '<div class="blog-post">' +
+                '<h3>{{ post.title }}</h3>' +
+                '<button v-on:click="$emit(\'enlarge-text\', 0.1)">' +
+                    'Increase font-size' +
+                '</button>' +
+                '<div v-html="post.content"></div>' +
+            '</div>'
+})
+
+
+new Vue({
+    el:"#app-3", 
+    data: {
+        posts: [
+            {
+                id: 1, 
+                title: "My journey with Vue",
+                content: '<p>One one one one</p><p>One1 one1 one1</p>'
+            },
+            {
+                id: 2, 
+                title: "Blogging with Vue",
+                content: '<p>Two two two</p><p>Two2 two2 two2</p>' 
+            },
+            {
+                id: 3, 
+                title: "Why Vue is so fun",
+                content: '<p>Three three three</p><p>Three3 three3 three3</p>'
+            }
+        ],
+        postFontSize: 1
+    },
+
+    //for increase font-size through method
+    methods: {
+        onEnlargeText: function (enlargeAmount) {
+            this.postFontSize +=enlargeAmount
+        }
+    }
+})
+
+
+//v-model Usage in Components
+Vue.component('custom-input', {
+    props: ["value"],
+    template: '<div><p>{{ value }}</p><input ' +
+                'v-bind:value="value" ' +
+                'v-on:input="$emit(\'input\', $event.target.value)"' +
+                '></div>' 
+})
+new Vue({
+    el: "#app-4",
+    data: {
+        searchText: "Enter phrase"
+    }
+})
+
+Vue.component("slots", {
+    template: '<div>' +
+                '<strong>Hello!</strong>' +
+                '<slot></slot>' + 
+            '</div>'
+})
+new Vue({
+    el:"#app-5"
+})
+
