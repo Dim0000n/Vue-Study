abe790fd1685d5a3b5100990d181ed07b2cfb56e
Dimon <yoh60438@gmail.com>
2023-01-24 14:04:06 +0700

    Start to study animation for lists

---
+++ Animation/ListsAnimation/index.js
@@ -0,0 +1,15 @@
+//Анимирование одиночного элемента/компонента
+new Vue({
+    el: "#app1",
+    data: {
+        show: true
+    }
+})
+
+//CSS-transition
+new Vue({
+    el: "#app2",
+    data: {
+        show: true
+    }
+})
