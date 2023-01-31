92c883202436aa0a85d51abe763e4096cb2843ed
Dimon <yoh60438@gmail.com>
2023-01-31 23:25:09 +0700

    learned animation between elements

---
+++ Animation/AnimationBetweenElements/index.js
@@ -0,0 +1,23 @@
+//v-if/v-else
+new Vue({
+    el: "#app1",
+    data: {
+        isEditing: false
+    }
+})
+
+//trinary condition
+new Vue({
+    el:"#app2",
+    data: {
+        isEditing: false
+    }
+})
+
+//out-in
+new Vue({
+    el:"#app3",
+    data: {
+        isEditing: false
+    }
+})
