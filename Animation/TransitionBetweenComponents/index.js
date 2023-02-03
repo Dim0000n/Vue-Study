2255a4ec0e18a151f6f98b41e85d5b5cfc177308
Dimon <yoh60438@gmail.com>
2023-02-03 21:52:59 +0700

    Learned TransitionBetweenComponents

---
+++ Animation/TransitionBetweenComponents/index.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#app1",
+    data: {
+        view: ''
+    },
+    components: {
+        'v-a': {
+            template: '<div>Component A</div>'
+        },
+        'v-b': {
+            template: '<div>Component B</div>'
+        }
+    }
+})
