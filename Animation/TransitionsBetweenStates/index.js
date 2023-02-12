10b8c2b78da9c730fbb83008db2ac95e404b8a7a
Dimon <yoh60438@gmail.com>
2023-02-12 22:16:46 +0700

    learned state animation via observer

--- Animation/TransitionsBetweenStates/index.js
+++ Animation/TransitionsBetweenStates/index.js
@@ -1,3 +1,4 @@
+//State animation via observer
 new Vue({
     el: "#app1",
     data: {
@@ -14,4 +15,61 @@ new Vue({
             gsap.to(this.$data, {duration: 0.5, tweenedNumber: newValue})   //animate via GreenSock
         }
     }
+})
+
+//CSS-color animation via observer
+var Color = net.brehaut.Color //from color-js library
+
+new Vue({
+    el: '#app2',
+    data: {
+        colorQuery: "",
+        color: {
+            red: 0,
+            green: 0,
+            blue: 0,
+            alpha: 1
+        },
+        tweenedColor: {}
+    },
+
+    //created-hook
+    created: function() {
+        this.tweenedColor = Object.assign({}, this.color) //copy object this.color to this.tweenedColor
+    },
+
+    //watch-hoot
+    watch: {
+        color: function() {
+            function animate () {
+                if (TWEEN.update()) {
+                    requestAnimationFrame(animate) //say to browser, that we want to animate
+                }
+            }
+            
+            //from tween-library
+            new TWEEN.Tween(this.tweenedColor)
+            .to(this.color, 750)
+            .start()
+            
+            animate()
+        }
+    },
+    computed: {
+        tweenedCSSColor: function() {
+            return new Color({
+                red: this.tweenedColor.red,
+                green: this.tweenedColor.green,
+                blue: this.tweenedColor.blue,
+                alpha: this.tweenedColor.alpha
+            }).toCSS()
+        }
+    },
+    methods: {
+        updateColor: function() {
+            this.color = new Color(this.colorQuery).toRGB()
+            this.colorQuery = ""
+        }
+    }
+
 })
